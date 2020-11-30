1d67d3a735167803d42476d20f16b76cb5f97c3d
sbraun18 <sbraun@zagmail.gonzaga.edu>
2020-11-29 21:42:32 -0800

    Initial Commit

---
+++ pa8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pa8
+//
+//  Created by Sophia Braun on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
